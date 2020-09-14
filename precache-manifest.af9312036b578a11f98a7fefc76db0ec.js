81439183b1f2ead6bf9431fc2793fdc5bc518577
piyush <piyushjoshi2519@gmail.com>
2020-09-14 17:19:48 +0530

    Updates

---
+++ precache-manifest.af9312036b578a11f98a7fefc76db0ec.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f4c199a2f3281fee752af6c2910b75",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "3a2f570e6eec89c449bf",
+    "url": "/React/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "5333b176b26594085627",
+    "url": "/React/static/css/main.cf0c48da.chunk.css"
+  },
+  {
+    "revision": "3a2f570e6eec89c449bf",
+    "url": "/React/static/js/2.ee875080.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/React/static/js/2.ee875080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5333b176b26594085627",
+    "url": "/React/static/js/main.be44ff72.chunk.js"
+  },
+  {
+    "revision": "c42671de69057b804cbc",
+    "url": "/React/static/js/runtime-main.8466f367.js"
+  },
+  {
+    "revision": "34784bd50f59a7e28d634b623404eb13",
+    "url": "/React/static/media/about.34784bd5.png"
+  },
+  {
+    "revision": "c4fde9e724a2b3b3a26b00963fe2db58",
+    "url": "/React/static/media/ana.c4fde9e7.jpg"
+  },
+  {
+    "revision": "d33e6194b9476c7524feb55a65abc714",
+    "url": "/React/static/media/android.d33e6194.jpg"
+  },
+  {
+    "revision": "0b5f5962435a92fa01118d3577231b7b",
+    "url": "/React/static/media/home.0b5f5962.jpg"
+  },
+  {
+    "revision": "f60b674b6ff921adabe0485719a16e11",
+    "url": "/React/static/media/homepage.f60b674b.jpg"
+  },
+  {
+    "revision": "519cb14326f2aadda44e8f43fa3da2f8",
+    "url": "/React/static/media/ml.519cb143.jpg"
+  },
+  {
+    "revision": "340891634eaca61edccc2643b3575279",
+    "url": "/React/static/media/s1.34089163.jpg"
+  },
+  {
+    "revision": "86dcc26e54aeb4da1001a2b9a32839bb",
+    "url": "/React/static/media/softeare.86dcc26e.jpg"
+  }
+]);
